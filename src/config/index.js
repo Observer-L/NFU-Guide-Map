e53e2a2d41250fe2fdbadeec2579f9efc21e6ce7
Observer-L <observer_l@163.com>
2019-06-01 14:15:46 +0800

    首页markers逻辑重写

---
+++ src/config/index.js
@@ -0,0 +1,7 @@
+import markerStyle from './markerStyle'
+
+export default {
+  version: '1.0.0',
+  dataVersion: 0,
+  markerStyle
+}
---
+++ src/config/markerStyle.js
@@ -0,0 +1,11 @@
+export default {
+  width: 28,
+  height: 36,
+  calloutStyle: {
+    display: 'ALWAYS',
+    bgColor: '#ffffff',
+    borderRadius: 3,
+    fontSize: 9,
+    padding: 3
+  }
+}
