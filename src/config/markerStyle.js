b0f00037d0bc5e8237f50f8446cacabada38b890
Observer-L <observer_l@163.com>
2019-06-02 14:24:18 +0800

    路线代码逻辑和样式改写

--- src/config/markerStyle.js
+++ src/config/markerStyle.js
@@ -7,5 +7,16 @@ export default {
     borderRadius: 3,
     fontSize: 9,
     padding: 3
+  },
+  polylineStyle: {
+    color: '#FFFFFF',
+    width: 4,
+    borderColor: '#1296DB',
+    borderWidth: 1
+  },
+  circleStyle: {
+    color: '#1296DB',
+    fillColor: '#1296DB50',
+    strokeWidth: 1
   }
 }
